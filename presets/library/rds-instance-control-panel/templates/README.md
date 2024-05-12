b0fca07f9a9d082e8db0a719b18a29c76bb891bd
Filip Pýrek <PyrekFilip@gmail.com>
2024-05-12 13:10:37 +0200

    feat: add rds control panel

---
+++ presets/library/rds-instance-control-panel/templates/README.md
@@ -0,0 +1,25 @@
+<p align="center">
+  <a href="https://buttonize.io">
+    <img width="350" alt="Buttonize.io" src="https://user-images.githubusercontent.com/6282843/212024942-9fd50774-ea26-48ba-b2cf-ca2584498c9a.png">
+  </a>
+</p>
+
+---
+
+## RDS Instance Control Panel
+
+[![RDS Instance Control Panel](https://github.com/buttonize/create-buttonize/assets/6282843/2b47ee3d-d4c6-442a-a366-acfc35765ab9)](https://buttonize.io/library/rds-instance-control-panel)
+
+Learn more about RDS Instance Control Panel Construct [here](https://buttonize.io/library/rds-instance-control-panel).
+
+## CDK
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+### Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npx cdk deploy`  deploy this stack to your default AWS account/region
+* `npx cdk diff`    compare deployed stack with current state
+* `npx cdk synth`   emits the synthesized CloudFormation template
---
+++ presets/library/rds-instance-control-panel/templates/lib/example-stack.ts
@@ -0,0 +1,39 @@
+import * as cdk from 'aws-cdk-lib'
+import {
+	InstanceClass,
+	InstanceSize,
+	InstanceType,
+	SubnetType,
+	Vpc
+} from 'aws-cdk-lib/aws-ec2'
+import { DatabaseInstance, DatabaseInstanceEngine } from 'aws-cdk-lib/aws-rds'
+import { Buttonize } from 'buttonize/cdk'
+import { RDSInstanceControlPanel } from 'buttonize/library'
+import { Construct } from 'constructs'
+
+export class ExampleStack extends cdk.Stack {
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props)
+
+		Buttonize.init(this, {
+			apiKey: '@@apiKey'
+		})
+
+		const defaultVpc = Vpc.fromLookup(this, 'VPC', { isDefault: true })
+
+		const instance = new DatabaseInstance(this, 'RdsInstance', {
+			engine: DatabaseInstanceEngine.POSTGRES,
+			vpc: defaultVpc,
+			instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MICRO),
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			instanceIdentifier: 'my-little-test-buttonize-db',
+			vpcSubnets: {
+				subnetType: SubnetType.PUBLIC // Publicly accessible instance
+			}
+		})
+
+		new RDSInstanceControlPanel(this, 'ControlPanel', {
+			instance
+		})
+	}
+}
